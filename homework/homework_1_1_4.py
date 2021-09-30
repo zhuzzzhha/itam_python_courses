77d7c946008d759864affadb80db0ef005df053a
zhuzzzhha <90785698+zhuzzzhha@users.noreply.github.com>
2021-09-30 19:42:02 +0300

    Update homework_1_1_4.py

--- homework/homework_1_1_4.py
+++ homework/homework_1_1_4.py
@@ -3,10 +3,10 @@ more_8=0
 negative=0
 even=0
 for i in range(len(a)):
-    if a[i]<0:
+    if int(a[i])<0:
         negative+=1
-    if a[i]>8:
+    if int(a[i])>8:
         more_8+=1
-    if a[i]%2==0:
+    if int(a[i])%2==0:
         even+=1
 print("больше 8 - ", more_8,"отрицательных - ", negative, "четных - ", even)
