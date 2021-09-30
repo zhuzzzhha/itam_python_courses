8f3025f8022b58e7f0d40e79108d8153412eda0b
zhuzzzhha <90785698+zhuzzzhha@users.noreply.github.com>
2021-09-30 19:40:32 +0300

    Create homework_1_2_1.py

---
+++ homework/homework_1_2_1.py
@@ -0,0 +1,13 @@
+n=int(input())
+och=0
+zaoch=0
+for i in range(n):
+    surname=input()
+    name=input()
+    age=int(input())
+    part=input()
+    if part:
+       och+=1
+    else:
+        zaoh+=1
+print(och,zaoch)
