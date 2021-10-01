991dc4fc6b70922dff730a8602dcebfbac6d5325
zhuzzzhha <90785698+zhuzzzhha@users.noreply.github.com>
2021-10-01 20:42:27 +0300

    Update 1_2_3.py

--- homework/1_2_3.py
+++ homework/1_2_3.py
@@ -1,1 +1,15 @@
 
+def split(s):
+    return [char for char in s]
+a=input()
+b=split(a)
+n=len(b)
+f=False
+for i in range(n):
+    if b[i].isupper():
+        j=i
+    if b[i].isdigit() and f==False:
+        k=i+1
+        f=True
+b=b[j:n:k-j]
+print(" ".join(b))
